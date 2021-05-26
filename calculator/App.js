0cd325119c9a0f10859f8a0216cfc8fb8cf62921
johannh22 <johannhomonnai@gmail.com>
2021-05-26 15:54:50 -0300

    Lógica da Calculadora #02

--- calculator/App.js
+++ calculator/App.js
@@ -13,15 +13,14 @@ const initialState = {
 
 export default class App extends Component {
   // ... pega todos os atributos. state é um clone de initialState
-  state = {...initialState}
+  state = { ...initialState }
 
   addDigit = n => {
     if (n === '.' && this.state.displayValue.includes('.')) {
       return // faz nada
     }
 
-    const clearDisplay = this.state.displayValue === '0'
-      || this.state.clearDisplay
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n // concatena, são strings
     this.setState({ displayValue, clearDisplay: false })
@@ -30,20 +29,40 @@ export default class App extends Component {
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[this.state.current] = newValue
+      this.setState({ values })
     }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: 0 })
+    this.setState({ ...initialState })
   }
 
   setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [ ...this.state.values ]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0: 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
   render() {
     return (
-      <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
           <Button label='AC' triple onClick={this.clearMemory} />
@@ -64,7 +83,7 @@ export default class App extends Component {
           <Button label='.' onClick={this.addDigit} />
           <Button label='=' operation onClick={this.setOperation} />
         </View>
-      </SafeAreaView>
+      </View>
     )
   }
 }
