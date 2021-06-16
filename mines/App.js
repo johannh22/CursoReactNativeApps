ea3cc785baeda86d66ef9eb3033bd5928ed6b09d
johannh22 <johannhomonnai@gmail.com>
2021-06-16 16:53:43 -0300

    LevelSelection. Jogo finalizado!

--- mines/App.js
+++ mines/App.js
@@ -10,6 +10,7 @@ import {
 import params from './src/params';
 import MineField from './src/components/MineField';
 import Header from './src/components/Header';
+import LevelSelection from './src/screens/LevelSelection';
 import {
   createMinedBoard,
   cloneBoard,
@@ -41,6 +42,7 @@ export default class App extends Component {
       board: createMinedBoard(rows, cols, this.minesAmount()),
       won: false,
       lost: false,
+      showLevelSelection: false,
     };
   };
 
@@ -74,12 +76,23 @@ export default class App extends Component {
     this.setState({board, won});
   };
 
+  onLevelSelected = level => {
+    params.difficultyLevel = level;
+    this.setState(this.createState);
+  };
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
+        <LevelSelection
+          isVisible={this.state.showLevelSelection}
+          onLevelSelected={this.onLevelSelected}
+          onCancel={() => this.setState({showLevelSelection: false})}
+        />
         <Header
           flagsLeft={this.minesAmount() - flagsUsed(this.state.board)}
           onNewGame={() => this.setState(this.createState)}
+          onFlagPress={() => this.setState({showLevelSelection: true})}
         />
         <View style={styles.board}>
           <MineField
