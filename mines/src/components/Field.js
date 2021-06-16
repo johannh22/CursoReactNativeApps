754d21cb11aa921d91b401bd7c1339e4f9581d6f
johannh22 <johannhomonnai@gmail.com>
2021-06-16 15:52:23 -0300

    Lógica e começo de openField

--- mines/src/components/Field.js
+++ mines/src/components/Field.js
@@ -1,65 +1,68 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import params from '../params'
-import Mine from './Mine'
-import Flag from './Flag'
+import React from 'react';
+import {View, StyleSheet, Text, TouchableWithoutFeedback} from 'react-native';
+import params from '../params';
+import Mine from './Mine';
+import Flag from './Flag';
 
 export default props => {
-    const { mined, open, nearMines, exploded, flagged } = props
+  const {mined, open, nearMines, exploded, flagged} = props;
 
-    const styleField = [styles.field]
-    if (open) styleField.push(styles.open)
-    if (exploded) styleField.push(styles.exploded)
-    if (flagged) styleField.push(styles.flagged)
-    if (!open && !exploded) styleField.push(styles.regular)
+  const styleField = [styles.field];
+  if (open) styleField.push(styles.open);
+  if (exploded) styleField.push(styles.exploded);
+  if (flagged) styleField.push(styles.flagged);
+  if (!open && !exploded) styleField.push(styles.regular);
 
-    let color = null
-    if (nearMines > 0) {
-        if (nearMines == 1) color = '#2A28D7'
-        if (nearMines == 2) color = '#2B520F'
-        if (nearMines > 2 && nearMines < 6) color = '#F9060A'
-        if (nearMines >= 6) color = '#F221A9'
-    }
+  let color = null;
+  if (nearMines > 0) {
+    if (nearMines == 1) color = '#2A28D7';
+    if (nearMines == 2) color = '#2B520F';
+    if (nearMines > 2 && nearMines < 6) color = '#F9060A';
+    if (nearMines >= 6) color = '#F221A9';
+  }
 
-    return (
-        <View style={styleField}>
-            {!mined && open && nearMines > 0 ?
-                <Text style={[styles.label, { color: color }]}>
-                    {nearMines}
-                </Text> : false}
-            {mined && open ? <Mine /> : false}
-            {flagged && !open ? <Flag /> : false}
-        </View>
-    )
-}
+  return (
+    <TouchableWithoutFeedback onPress={props.onOpen}>
+      <View style={styleField}>
+        {!mined && open && nearMines > 0 ? (
+          <Text style={[styles.label, {color: color}]}>{nearMines}</Text>
+        ) : (
+          false
+        )}
+        {mined && open ? <Mine /> : false}
+        {flagged && !open ? <Flag /> : false}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 const styles = StyleSheet.create({
-    field: {
-        height: params.blockSize,
-        width: params.blockSize,
-        borderWidth: params.borderSize,
-    },
-    regular: {
-        borderWidth: 2,
-        backgroundColor: '#999',
-        borderLeftColor: '#CCC',
-        borderTopColor: '#CCC',
-        borderRightColor: '#333',
-        borderBottomColor: '#333',
-    },
-    open: {
-        borderWidth: 2,
-        backgroundColor: '#999',
-        borderColor: '#777',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    label: {
-        fontWeight: 'bold',
-        fontSize: params.fontSize,
-    },
-    exploded: {
-        backgroundColor: 'red',
-        borderColor: 'red',
-    },
-})
+  field: {
+    height: params.blockSize,
+    width: params.blockSize,
+    borderWidth: params.borderSize,
+  },
+  regular: {
+    borderWidth: 4,
+    backgroundColor: '#999',
+    borderLeftColor: '#CCC',
+    borderTopColor: '#CCC',
+    borderRightColor: '#333',
+    borderBottomColor: '#333',
+  },
+  open: {
+    borderWidth: 2,
+    backgroundColor: '#999',
+    borderColor: '#777',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: 'red',
+    borderColor: 'red',
+  },
+});
--- mines/src/functions.js
+++ mines/src/functions.js
@@ -24,20 +24,86 @@ const spreadMines = (board, minesAmount) => {
   let minesPlanted = 0;
 
   while (minesPlanted < minesAmount) {
-    const rowSel = parseInt(Math.random() * rows, 10)
-    const columnSel = parseInt(Math.random() * columns, 10)
+    const rowSel = parseInt(Math.random() * rows, 10);
+    const columnSel = parseInt(Math.random() * columns, 10);
 
     if (!board[rowSel][columnSel].mined) {
-      board[rowSel][columnSel].mined = true
-      minesPlanted++
+      board[rowSel][columnSel].mined = true;
+      minesPlanted++;
     }
   }
 };
 
 const createMinedBoard = (rows, columns, minesAmount) => {
-  const board = createBoard(rows, columns)
-  spreadMines(board, minesAmount)
-  return board
-}
+  const board = createBoard(rows, columns);
+  spreadMines(board, minesAmount);
+  return board;
+};
 
-export { createMinedBoard }
+const cloneBoard = board => {
+  return board.map(rows => {
+    return rows.map(field => {
+      return {...field};
+    });
+  });
+};
+
+const getNeighbors = (board, row, column) => {
+  const neighbors = [];
+  const rows = [row - 1, row, row + 1];
+  const columns = [column - 1, column, column + 1];
+  rows.forEach(r => {
+    columns.forEach(c => {
+      const different = r !== row || c !== column;
+      const validRow = r >= 0 && r < board.length;
+      const validColumn = c >= 0 && c < board[0].length;
+      if (different && validRow && validColumn) {
+        neighbors.push(board[r][c]);
+      }
+    });
+  });
+  return neighbors;
+};
+
+const safeNeighborhood = (board, row, column) => {
+  const safes = (result, neighbor) => result && !neighbor.mined;
+  return getNeighbors(board, row, column).reduce(safes, true);
+};
+
+const openField = (board, row, column) => {
+  const field = board[row][column];
+  if (!field.open) {
+    field.open = true;
+    if (field.mined) {
+      field.exploded = true;
+    } else if (safeNeighborhood(board, row, column)) {
+      getNeighbors(board, row, column).forEach(n =>
+        openField(board, n.row, n.column),
+      );
+    } else {
+      const neighbors = getNeighbors(board, row, column);
+      field.nearMines = neighbors.filter(n => n.mined).length;
+    }
+  }
+};
+
+// Recebe um array de boards e os concatena
+const fields = board => [].concat(...board);
+const hasExplosion = board =>
+  fields(board).filter(field => field.exploded).length > 0;
+const pending = field =>
+  (field.mined && !field.flagged) || (!field.mined && !field.open);
+const wonGame = board => fields(board).filter(pending).length === 0;
+const showMines = board =>
+  field(board)
+    .filter(field => field.mined)
+    .forEach(field => (field.open = true));
+
+export {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hasExplosion,
+  wonGame,
+  showMines,
+};
